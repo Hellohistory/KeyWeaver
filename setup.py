1558b32288040081d36541e15ccd1c05909b46cd
Hellohistory <85825011+Hellohistory@users.noreply.github.com>
2025-08-13 14:49:06 +0800

    fix: workflow

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
-import sys
 import os
 from cx_Freeze import setup, Executable
 
 VERSION = os.environ.get('CI_VERSION', '0.0.0')
 
-if sys.platform == "darwin":
-    icon_path = "logo.icns"
-    base = None
-elif sys.platform == "win32":
-    icon_path = "logo.ico"
-    base = "Win32GUI"
-else:
-    icon_path = None
-    base = None
+base = "Win32GUI"
+icon_path = "logo.ico"
 
 main_script = "main.py"
 executable_name = "KeyWeaver"
@@ -20,7 +12,6 @@ executable_name = "KeyWeaver"
 include_files = [
     'logo.png',
     'logo.ico',
-    'logo.icns',
 ]
 
 packages = ["PyQt5", "fastapi", "uvicorn", "zxcvbn", "pyperclip", "asyncio"]
@@ -39,17 +30,12 @@ build_exe_options = {
     ],
 }
 
-bdist_mac_options = {
-    "bundle_name": executable_name,
-}
-
 setup(
     name="KeyWeaver",
     version=VERSION,
     description="A powerful password generator and strength checker.",
     options={
         "build_exe": build_exe_options,
-        "bdist_mac": bdist_mac_options,
     },
     executables=[
         Executable(
